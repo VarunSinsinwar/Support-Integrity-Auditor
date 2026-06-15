ab21b606bf9bd9ef493029c2cd245de48534db8f
Varun Sinsinwar <143028242+VarunSinsinwar@users.noreply.github.com>
2026-06-15 13:07:59 +0530

    Update predict.py

--- predict.py
+++ predict.py
@@ -7,20 +7,20 @@ from transformers import AutoTokenizer, AutoModelForSequenceClassification
 HF_REPO_ID = "sinsinwarvarun/SIA-DistilBERT-finetuned" 
 
 class SupportIntegrityAuditor:
-   def __init__(self):
-    print("Loading tokenizer from Hugging Face Hub...")
+    def __init__(self):
+         print("Loading tokenizer from Hugging Face Hub...")
 
-    self.tokenizer = AutoTokenizer.from_pretrained(
-        HF_REPO_ID
-    )
+       self.tokenizer = AutoTokenizer.from_pretrained(
+            HF_REPO_ID
+       )
 
-    print("Loading model from Hugging Face Hub...")
-
-    self.model = AutoModelForSequenceClassification.from_pretrained(
-        HF_REPO_ID
-    )
-
-    self.model.eval()
+       print("Loading model from Hugging Face Hub...")
+   
+       self.model = AutoModelForSequenceClassification.from_pretrained(
+           HF_REPO_ID
+       )
+   
+       self.model.eval()
 
     def _extract_rule_flags(self, text, priority):
         """
